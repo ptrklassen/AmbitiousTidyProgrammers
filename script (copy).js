c22e10deb16da934638cbf4e5902ca65fb2ad952
Peter Klassen <ptrklassen@gmail.com>
2023-07-28 19:27:48 +0000

    second sprite animation

--- script (copy).js
+++ script (copy).js
@@ -3,32 +3,27 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 20;
+const numberOfEnemies = 15;
 const enemiesArray = [];
 let gameFrame = 0;
 
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = './assets/enemy2.png';  
-    this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 266;
-    this.spriteHeight = 188;
-    this.width = this.spriteWidth / 2;
-    this.height = this.spriteHeight / 2;
+    this.image.src = './assets/enemy1.png';  
+    // this.speed = Math.random() * 4 - 2;
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-    this.angle = Math.random() * 2;
-    this.angleSpeed = Math.random() * 0.2;
-    this.curve = Math.random() * 7 ;
   }
   update () {
-    this.x -= this.speed;
-    this.y += this.curve * Math.sin(this.angle);
-    this.angle += this.angleSpeed;
-    if(this.x + this.width < 0) this.x = canvas.width; 
+    this.x += Math.random() * 5 - 2.5;
+    this.y += Math.random() * 5 - 2.5;
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? this.frame = 0 : this.frame++;
     };
