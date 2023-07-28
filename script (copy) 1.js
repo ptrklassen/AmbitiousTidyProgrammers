41097ecafd74504df006803434b19784d5fb2f97
Peter Klassen <ptrklassen@gmail.com>
2023-07-28 19:53:18 +0000

    third sprite animation

--- script (copy) 1.js
+++ script (copy) 1.js
@@ -10,10 +10,10 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = './assets/enemy3.png';  
+    this.image.src = './assets/enemy2.png';  
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 218;
-    this.spriteHeight = 177;
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
     this.width = this.spriteWidth / 2;
     this.height = this.spriteHeight / 2;
     this.x = Math.random() * (canvas.width - this.width);
@@ -21,15 +21,14 @@ class Enemy {
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     this.angle = Math.random() * 2;
-    this.angleSpeed = Math.random() * 1.5 + 0.5;
-    // this.curve = Math.random() * 200 + 50;
+    this.angleSpeed = Math.random() * 0.2;
+    this.curve = Math.random() * 7 ;
   }
   update () {
-    this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/90) + (canvas.width / 2 - this.width / 2);
-    this.y = canvas.height/2 * Math.cos(this.angle * Math.PI/270) + (canvas.height / 2 - this.height / 2);
+    this.x -= this.speed;
+    this.y += this.curve * Math.sin(this.angle);
     this.angle += this.angleSpeed;
     if(this.x + this.width < 0) this.x = canvas.width; 
-    // animate sprites
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? this.frame = 0 : this.frame++;
     };
